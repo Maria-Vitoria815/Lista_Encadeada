ac78ff660582275652cd622422b2eabe39f1fc1b
Maria-Vitoria815 <vitoriamellomv028@outlook.com>
2024-08-26 17:10:54 -0300

    ATU

--- ListaDuplamenteEncadeada/src/classes/Principal.java
+++ ListaDuplamenteEncadeada/src/classes/Principal.java
@@ -7,8 +7,7 @@ public class Principal {
 		Lista l = new Lista();
 		
 		    l.adicionarNoInicio("Rodrigo Santoro");
-	      
-	        System.out.println("===== Listagem =====");
+	        System.out.println("===== Lista =====");
 	        l.imprimir();
 	        
 	        
@@ -18,46 +17,42 @@ public class Principal {
 			l.adicionarNoInicio("Mariane Melo");
 	      
 	        System.out.println("\n\n");
-	        System.out.println("===== Lista Atualizada =====");
+	        System.out.println("===== Lista após adicionar no início=====");
 	        l.imprimir(); 
 	        
 	     
 	        
 	        l.adicionarNoFim("Emily Louise");
-	        
 	        System.out.println("\n\n");
-	        System.out.println("===== Lista Atualizada =====");
+	        System.out.println("===== Lista após adicionar no fim =====");
 	        l.imprimir();
 
 
 
 			l.removerDoInicio();
-	        
 	        System.out.println("\n\n");
-	        System.out.println("===== Lista Atualizada =====");
+	        System.out.println("===== Lista após remover do início =====");
 	        l.imprimir();
 
 
 
 			//l.removerDoFinal();
-	        
 	       // System.out.println("\n\n");
-	       // System.out.println("===== Lista Atualizada =====");
+	       // System.out.println("===== Lista após remover do final =====");
 	       // l.imprimir();
 	        
 	        
+
 		   l.adicionarPorPosicao(3, "Cayo Gabriel");
-	        
 		   System.out.println("\n\n");
-		   System.out.println("===== Lista Atualizada =====");
+		   System.out.println("===== Lista após adicionar elemento na posição 3 =====");
 		   l.imprimir();
 	        
 
 
 		   l.removerPorPosicao(4);
-	        
 		   System.out.println("\n\n");
-		   System.out.println("===== Lista Atualizada =====");
+		   System.out.println("===== Lista após remover elemento da posição 4 =====");
 		   l.imprimir();
 	        
 	}
